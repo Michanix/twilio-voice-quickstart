cf1595d3813e845731825552c6ee5cb7b89d87d4
mihhail.m <mihhail.matisinets@gmail.com>
2021-01-21 14:14:48 +0200

    MakePhoneCall added, first test run

--- src/main/java/com/assignment/Main.java
+++ src/main/java/com/assignment/Main.java
@@ -1,7 +1,25 @@
 package com.assignment;
 
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Call;
+import com.twilio.type.PhoneNumber;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Test");
+    public static void main(String[] args) throws URISyntaxException {
+        MakePhoneCall phoneCall = new MakePhoneCall();
+        Twilio.init(phoneCall.getACC_SID(), phoneCall.getAUTH_TOKEN());
+
+        // Phone numbers should be in E.164 format
+        // more info: https://www.twilio.com/docs/glossary/what-e164
+        String from = "+12013409432";
+        String to = "+37255618061";
+
+        Call call = Call.creator(new PhoneNumber(to), new PhoneNumber(from),
+                new URI("http://demo.twilio.com/docs/voice.xml*")).create();
+        // printing account SID to confirm, that phone call succeeded
+        System.out.println(call.getAccountSid());
     }
 }
---
+++ src/main/java/com/assignment/MakePhoneCall.java
@@ -0,0 +1,14 @@
+package com.assignment;
+
+public class MakePhoneCall {
+    private String ACC_SID = "ACebedb2f34511bfd203f64dd489ab95e1";
+    private String AUTH_TOKEN = "4c06d2565a1a6baa206004559d5b975e";
+
+    public String getACC_SID() {
+        return ACC_SID;
+    }
+
+    public String getAUTH_TOKEN() {
+        return AUTH_TOKEN;
+    }
+}
